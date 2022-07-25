ef5a5b3d79ed240b2f20590d21220423b4285034
Skorohod Andrew <pmisa376@gmail.com>
2022-07-25 21:43:20 +0300

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+
+const listFoto = document.querySelector(".gallery");
+const cardsFoto = createFotoCard(galleryItems);
+
+listFoto.insertAdjacentHTML("beforeend", cardsFoto);
+
+
+function createFotoCard(fotos) {
+  return fotos
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+          <img
+          class="gallery__image"
+          src="${preview}" 
+          alt="${description}"
+           />
+      </a>
+      `;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 console.log(galleryItems);
