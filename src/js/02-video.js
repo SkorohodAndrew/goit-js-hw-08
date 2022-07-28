c88628bef223f3e7e1161eb55cd3b3c5ddc94203
Skorohod Andrew <pmisa376@gmail.com>
2022-07-28 15:12:14 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,15 +4,13 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(onFormSubmit, 1000)), player;
-
 function onFormSubmit(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
   console.log(data.seconds);
 }
 
 const timeVideo = localStorage.getItem('videoplayer-current-time');
-
+player.on('timeupdate', throttle(onFormSubmit, 1000)), player;
 player
   .setCurrentTime(timeVideo)
   .then(function (seconds) {})
