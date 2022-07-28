34f737a5d6449f43f82c49c95e2ad228a36fbdf3
Skorohod Andrew <pmisa376@gmail.com>
2022-07-28 15:07:02 +0300

    fix 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onFormSubmit, 1000)), player;
+
+function onFormSubmit(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  console.log(data.seconds);
+}
+
+const timeVideo = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(timeVideo)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        n;
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const STORAGE_KEY = 'feedback-form-state';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.input.addEventListener('input', onTextareaInput);
+refs.input.addEventListener('input', onTextareaInput);
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  evt.currentTarget.reset();
+
+  console.log('відправити форму');
+}
+
+function onTextareaInput(evt) {
+  const message = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, message);
+}
+
+function populateTextarea(evt) {
+  const saveMassage = localStorage.getItem(STORAGE_KEY);
+
+  console.log(saveMassage);
+}
